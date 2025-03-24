ddbea2f8224db0fd027e5037a1edfc5bea63e942
levin256 <28007281+levin256@users.noreply.github.com>
2025-03-24 22:20:29 +0900

    initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,247 @@
+import { useEffect, useRef, useState } from 'react';
+import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { escapeRegExp } from './utils/string';
+
+const App = () => {
+  const excludeInogreString = (text: string) => {
+    const textWithoutIgnoreLine = ignoreLines.reduce(
+      (currentText, ignoreLine) => {
+        const lines = currentText.split(/\n/);
+        return lines.filter((line) => !line.startsWith(ignoreLine)).join('\n');
+      },
+      text,
+    );
+
+    const currentIgnoreStrings = [...ignoreStrings];
+    if (isIgnoreSpace) {
+      currentIgnoreStrings.push(' ');
+      currentIgnoreStrings.push('　');
+    }
+    if (isIgnoreLineBreak) {
+      currentIgnoreStrings.push('\n');
+    }
+    const textWithoutIgnoreString = currentIgnoreStrings.reduce(
+      (currentText, string) => {
+        const escapeString = escapeRegExp(string);
+        const regex = new RegExp(escapeString, 'g');
+        return currentText.replaceAll(regex, '');
+      },
+      textWithoutIgnoreLine,
+    );
+    return textWithoutIgnoreString;
+  };
+
+  const ignoreStringRef = useRef<HTMLInputElement>(null!);
+  const ignoreLineRef = useRef<HTMLInputElement>(null!);
+  const [isIgnoreSpace, setIsIgnoreSpace] = useState<boolean>(false);
+  const [isIgnoreLineBreak, setIsIgnoreLineBreak] = useState<boolean>(false);
+  const [ignoreStrings, setIgnoreStrings] = useState<string[]>([
+    '、',
+    '。',
+    '「',
+    '」',
+    '…',
+    '！',
+    '？',
+  ]);
+  const [ignoreLines, setIgnoreLines] = useState<string[]>(['//']);
+  const [text, setText] = useState('');
+  const [textCount, setTextCount] = useState(excludeInogreString(text).length);
+
+  useEffect(() => {
+    const lsIgnoreStrings = localStorage.getItem('ignoreStrings');
+    if (lsIgnoreStrings) {
+      setIgnoreStrings(JSON.parse(lsIgnoreStrings));
+    }
+
+    const lsIgnoreLines = localStorage.getItem('ignoreLines');
+    if (lsIgnoreLines) {
+      setIgnoreLines(JSON.parse(lsIgnoreLines));
+    }
+
+    const handleBeforeUnload = (event: Event) => {
+      event.preventDefault();
+    };
+    window.addEventListener('beforeunload', handleBeforeUnload);
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, []);
+
+  useEffect(() => {
+    setTextCount(excludeInogreString(text).length);
+  }, [text, isIgnoreSpace, isIgnoreLineBreak, ignoreStrings, ignoreLines]);
+
+  const isValidInput = (string: string) => {
+    if (!string) {
+      alert('文字を入力してください');
+      return false;
+    }
+    // TODO: なぜか機能しない
+    if (['\n'].includes(string)) {
+      alert('改行を指定できません。');
+      return false;
+    }
+    if (string.match(/^(\s|　)+$/)) {
+      alert('空白を指定できません。');
+      return false;
+    }
+    return true;
+  };
+
+  const addIgnoreString = () => {
+    const inputIgnoreString = ignoreStringRef.current?.value;
+    if (!isValidInput(inputIgnoreString)) return;
+
+    const newIgnoreStrings = [...ignoreStrings, inputIgnoreString];
+    setIgnoreStrings(newIgnoreStrings);
+    localStorage.setItem('ignoreStrings', JSON.stringify(newIgnoreStrings));
+    ignoreStringRef.current.value = '';
+  };
+
+  const addIgnoreLine = () => {
+    const inputIgnoreLine = ignoreLineRef.current.value;
+    if (!isValidInput(inputIgnoreLine)) return;
+
+    const newIgnoreLines = [...ignoreLines, inputIgnoreLine];
+    setIgnoreLines(newIgnoreLines);
+    localStorage.setItem('ignoreLines', JSON.stringify(newIgnoreLines));
+    ignoreLineRef.current.value = '';
+  };
+
+  const deleteIgnoreString = (deleteKeyIndex: number) => {
+    const afterDeleteignoreStrings = ignoreStrings.filter(
+      (_, index) => index !== deleteKeyIndex,
+    );
+    setIgnoreStrings(afterDeleteignoreStrings);
+    localStorage.setItem(
+      'ignoreStrings',
+      JSON.stringify(afterDeleteignoreStrings),
+    );
+  };
+
+  const deleteIgnoreLine = (deleteKeyIndex: number) => {
+    const afterDeleteignoreLines = ignoreLines.filter(
+      (_, index) => index !== deleteKeyIndex,
+    );
+    setIgnoreLines(afterDeleteignoreLines);
+    localStorage.setItem('ignoreLines', JSON.stringify(afterDeleteignoreLines));
+  };
+
+  return (
+    <div className="flex-column gap05rem">
+      <h1>台詞カウンター</h1>
+      <details>
+        <summary>説明をみる（クリックで展開）</summary>
+        <fieldset>
+          <p>
+            <b>テキスト</b>
+            に入力された文字数をリアルタイムにカウントするツールです。
+            <br />
+            カウントには、以下のルールを設定できます。
+          </p>
+          <h3>特殊文字のカウント設定</h3>
+          <ul>
+            <li>空白・改行をカウントするかの設定ができます</li>
+          </ul>
+          <h3>カウントしない文字</h3>
+          <ul>
+            <li>指定した文字はカウントされません</li>
+          </ul>
+          <h3>カウントしない行</h3>
+          <ul>
+            <li>指定した文字で始まる行はカウントされません</li>
+          </ul>
+        </fieldset>
+      </details>
+      <fieldset className="display-flex gap1rem">
+        <legend>特殊文字のカウント設定</legend>
+        <label>
+          <input
+            type="checkbox"
+            checked={isIgnoreSpace}
+            onChange={(event) => setIsIgnoreSpace(event.target.checked)}
+          />
+          空白をカウントしない
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            checked={isIgnoreLineBreak}
+            onChange={(event) => setIsIgnoreLineBreak(event.target.checked)}
+          />
+          改行をカウントしない
+        </label>
+      </fieldset>
+      <fieldset>
+        <legend>カウントしない文字</legend>
+        <ul>
+          {ignoreStrings.map((ignoreString, index) => (
+            <li key={index}>
+              <div className="display-flex padding-block">
+                <code className="code-label">{ignoreString}</code>
+                <button
+                  className="trash-btn"
+                  onClick={() => deleteIgnoreString(index)}
+                >
+                  <FontAwesomeIcon icon={faTrash} />
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <div className="btn-container">
+          <input
+            type="text"
+            placeholder="Enter ignore string."
+            ref={ignoreStringRef}
+          />
+          <button onClick={addIgnoreString}>
+            <FontAwesomeIcon icon={faPlus} />
+          </button>
+        </div>
+      </fieldset>
+      <fieldset>
+        <legend>カウントしない行</legend>
+        <ul>
+          {ignoreLines.map((ignoreLine, index) => (
+            <li key={index}>
+              <div className="display-flex padding-block">
+                <code className="code-label">{ignoreLine}</code>
+                <button
+                  className="trash-btn"
+                  onClick={() => deleteIgnoreLine(index)}
+                >
+                  <FontAwesomeIcon icon={faTrash} />
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <div className="btn-container">
+          <input
+            type="text"
+            placeholder="Enter ignore line."
+            ref={ignoreLineRef}
+          />
+          <button className="add-btn" onClick={addIgnoreLine}>
+            <FontAwesomeIcon icon={faPlus} />
+          </button>
+        </div>
+      </fieldset>
+      <div className="flex-column">
+        <h3>テキスト（文字数: {textCount}）</h3>
+        <textarea
+          className="radius-6p flex-column height-500"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Enter text."
+        />
+      </div>
+    </div>
+  );
+};
+
+export default App;
