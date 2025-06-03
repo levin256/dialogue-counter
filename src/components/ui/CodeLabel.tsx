6b75d7fb34d6f526a6fc602acd63eb7e5406b4f7
levin256 <28007281+levin256@users.noreply.github.com>
2025-06-04 02:13:27 +0900

    fix: CodeLabelが縦方向にセンタリングされるように修正

--- src/components/ui/CodeLabel.tsx
+++ src/components/ui/CodeLabel.tsx
@@ -6,7 +6,7 @@ export type CodeLabelProps = {
 export const CodeLabel = ({ label, className = '' }: CodeLabelProps) => {
   return (
     <code
-      className={`flex-1 overflow-hidden text-ellipsis whitespace-nowrap rounded-s-md bg-neutral-700 px-2 py-1 text-left text-sm underline decoration-neutral-400 underline-offset-4 ${className}`}
+      className={`flex flex-1 items-center overflow-hidden text-ellipsis whitespace-nowrap rounded-s-md bg-neutral-700 px-2 py-1 text-left text-sm underline decoration-neutral-400 underline-offset-4 ${className}`}
     >
       {label}
     </code>
